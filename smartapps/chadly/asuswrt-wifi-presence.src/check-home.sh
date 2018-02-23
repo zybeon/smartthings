d56e20ef58314a8c73be4741f696bec021ce56dc
zybeon <yates.rodney@gmail.com>
2018-02-23 16:50:00 -0500

    Update check-home.sh

--- smartapps/chadly/asuswrt-wifi-presence.src/check-home.sh
+++ smartapps/chadly/asuswrt-wifi-presence.src/check-home.sh
@@ -1,48 +1,70 @@
 #!/bin/sh
 
-Chad=Away
-Alanna=Away
+Rodney=Away
+Virginia=Away
+Sarah=Away
 
 macaddress="`wl -i eth1 assoclist` `wl -i eth2 assoclist` `wl -i eth3 assoclist`"
 
 case "$macaddress" in
 	*XX:XX:XX:XX:XX:XX*)
-	Chad=Home
+	Rodney=Home
 	;;
 esac
 
 case "$macaddress" in
 	*XX:XX:XX:XX:XX:XX*)
-	Alanna=Home
+	Virginia=Home
 	;;
 esac
 
-if [ "$Chad" = Home ]
+case "$macaddress" in
+	*XX:XX:XX:XX:XX:XX*)
+	Sarah=Home
+	;;
+esac
+
+if [ "$Rodney" = Home ]
 then
 	if [ ! -f /jffs/scripts/users/chad ]
 	then
-		touch /jffs/scripts/users/chad
+		touch /jffs/scripts/users/rodney
 		curl -H "Authorization: Bearer <ACCESS_TOKEN>" -X POST "https://<BASE_URL>/api/smartapps/installations/<APP_ID>/home"
 	fi
 else
 	if [ -f /jffs/scripts/users/chad ]
 	then
-		rm -f /jffs/scripts/users/chad
+		rm -f /jffs/scripts/users/rodney
 		curl -H "Authorization: Bearer <ACCESS_TOKEN>" -X POST "https://<BASE_URL>/api/smartapps/installations/<APP_ID>/away"
 	fi
 fi
 
-if [ "$Alanna" = Home ]
+if [ "$Virignia" = Home ]
 then
 	if [ ! -f /jffs/scripts/users/alanna ]
 	then
-		touch /jffs/scripts/users/alanna
+		touch /jffs/scripts/users/virginia
 		curl -H "Authorization: Bearer <ACCESS_TOKEN>" -X POST "https://<BASE_URL>/api/smartapps/installations/<APP_ID>/home"
 	fi
 else
 	if [ -f /jffs/scripts/users/alanna ]
 	then
-		rm -f /jffs/scripts/users/alanna
+		rm -f /jffs/scripts/users/virginia
+		curl -H "Authorization: Bearer <ACCESS_TOKEN>" -X POST "https://<BASE_URL>/api/smartapps/installations/<APP_ID>/away"
+	fi
+fi
+
+if [ "$Sarah" = Home ]
+then
+	if [ ! -f /jffs/scripts/users/alanna ]
+	then
+		touch /jffs/scripts/users/sarah
+		curl -H "Authorization: Bearer <ACCESS_TOKEN>" -X POST "https://<BASE_URL>/api/smartapps/installations/<APP_ID>/home"
+	fi
+else
+	if [ -f /jffs/scripts/users/alanna ]
+	then
+		rm -f /jffs/scripts/users/sarah
 		curl -H "Authorization: Bearer <ACCESS_TOKEN>" -X POST "https://<BASE_URL>/api/smartapps/installations/<APP_ID>/away"
 	fi
 fi
