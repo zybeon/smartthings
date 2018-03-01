4bdb7fb4c5881d6381b38f71c038e1afc18584d7
zybeon <yates.rodney@gmail.com>
2018-03-01 13:59:56 -0500

    Update init-start.sh

--- smartapps/chadly/asuswrt-wifi-presence.src/init-start.sh
+++ smartapps/chadly/asuswrt-wifi-presence.src/init-start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cru a CheckIfHome "* * * * * /jffs/scripts/check-home"
-cru a CheckIfHome15 "* * * * * sleep 15; /jffs/scripts/check-home"
-cru a CheckIfHome30 "* * * * * sleep 30; /jffs/scripts/check-home"
-cru a CheckIfHome45 "* * * * * sleep 45; /jffs/scripts/check-home"
+cru a CheckIfHome "* * * * * /opt/scripts/check-home"
+cru a CheckIfHome15 "* * * * * sleep 15; /opt/scripts/check-home"
+cru a CheckIfHome30 "* * * * * sleep 30; /opt/scripts/check-home"
+cru a CheckIfHome45 "* * * * * sleep 45; /opt/scripts/check-home"
