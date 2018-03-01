98596f0d90b46ee91cb2a573fab6047a2d0ad922
zybeon <yates.rodney@gmail.com>
2018-03-01 13:58:05 -0500

    Update router-wifi-presence.groovy

--- smartapps/chadly/asuswrt-wifi-presence.src/router-wifi-presence.groovy
+++ smartapps/chadly/asuswrt-wifi-presence.src/router-wifi-presence.groovy
@@ -1,25 +1,10 @@
 /**
- *  Copyright 2016 Chad Lee
- *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License. You may obtain a copy of the License at:
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
- *  for the specific language governing permissions and limitations under the License.
- *
- *	AsusWRT Wifi Presence
- *
- *	Author: Chad Lee, Based on work from Stuart Buchanan, Based on original work by midyear66 with thanks
- *
- *	2016-07-13: Initial Release
+
  */
 definition(
 	name: "Router Wifi Presence",
-	namespace: "chadly",
-	author: "Chad Lee",
+	namespace: "zybeon",
+	author: "zybeon",
 	description: "Triggers Wifi Presence Status when HTTP GET Request is recieved",
 	category: "My Apps",
 	iconUrl: "http://icons.iconarchive.com/icons/icons8/windows-8/256/Network-Wifi-icon.png",
